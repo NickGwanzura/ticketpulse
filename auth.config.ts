6692b92785e7686c4a4c9717f04897bf74fe2422
fevrose <fevrose@Nick-T-Gwanzura.local>
2026-07-01 15:44:31 +0200

    fix type casting for emailVerified in auth config

--- auth.config.ts
+++ auth.config.ts
@@ -25,7 +25,7 @@ export const authConfig = {
       if (token) {
         session.user.role = token.role as string
         session.user.id = token.id as string
-        session.user.emailVerified = token.emailVerified as string | null
+        session.user.emailVerified = token.emailVerified as unknown as string | null
       }
       return session
     },
