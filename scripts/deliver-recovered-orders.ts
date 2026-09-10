bf92c66492cd20097ca2ba901a811143eb00d619
fevrose <fevrose@users.noreply.github.com>
2026-09-10 13:28:32 +0200

    Allow slower remote connections for recovery delivery CLI

--- scripts/deliver-recovered-orders.ts
+++ scripts/deliver-recovered-orders.ts
@@ -2,6 +2,8 @@ import { dbPool } from "../db"
 import { deliverTicketForPaidOrder } from "../lib/delivery"
 
 async function main() {
+  // Remote operator connections can be slower than the application's server.
+  if (dbPool) dbPool.options.connectionTimeoutMillis = 20_000
   for (const id of process.argv.slice(2)) {
     console.log(JSON.stringify({ id, ...await deliverTicketForPaidOrder(id, { notifyOrganizers: false }) }))
   }
